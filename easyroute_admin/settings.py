bb3e4fb98c5b099a1308f5a2ce5647b89ae73755
bevenky <venky@miglu.com>
2011-04-18 12:58:03 +0530

    Update URL Conf

--- easyroute_admin/settings.py
+++ easyroute_admin/settings.py
@@ -100,7 +100,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
 )
 
-ROOT_URLCONF = 'easyroute.urls'
+ROOT_URLCONF = 'easyroute_admin.urls'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
